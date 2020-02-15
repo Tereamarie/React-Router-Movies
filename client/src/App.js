9828364cedeb808dcde6068b8c417de143ec04c9
marie-hotchkiss <mtknowlton67@gmail.com>
2020-02-14 19:01:52 -0800

    added style to moviecard

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom'
+import MovieList from './Movies/MovieList'
 import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import {Route, Link} from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    const saved = savedList.find(el => el.title === movie.title)
+    if (!saved) {
+      setSavedList( [...savedList, movie] );
+    } 
   };
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <Route exact path="/" component = {MovieList} />
-      <Route path="/movies/:id" render={ (props) => {
-        return(<Movie {...props} addToSavedList={addToSavedList}/>)
-      }} />
+      <Link to='/savedList'><SavedList list={savedList} /></Link>
+      <Route exact path = "/" component = {MovieList}/>
+      <Route path="/movies/:id" render={props => <Movie {...props} addToSavedList={addToSavedList} savedList={savedList} />}/>
     </div>
   );
-   
 };
 
-
-export default App
+export default App;
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const SavedList = (props) => {
- 
-console.log(props)
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></Link>
-        ))}
-        <Link to='/' className="home-button">Home</Link>
-        
-      
-      </div>
-    );
-  }
-export default SavedList
+const SavedList = props => (
+  <div className="saved-list">
+    <h3>Saved Movies:</h3>
+    {props.list.map(movie => (
+      <span className="saved-movie">{movie.title}</span>
+    ))}
+    <Link to="/" className="home-button">
+      Home
+    </Link>
+    <Link to="/clear-saved-list" onClick="clear-list">Clear</Link>
+  </div>
+);
+
+export default SavedList;
